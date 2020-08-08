b72bf85645ac74a9f9e275a85c72e0bd5833aede
Boran-AngryBacon <boran.birsan@gmail.com>
2020-08-08 22:38:21 +0300

    test run

--- Chatty.js
+++ Chatty.js
@@ -1,1 +1,27 @@
-const Discord = require('discord');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag);
+    console.log("Servers:");
+    client.guilds.cache.forEach((guild) => {
+        console.log(" - " + guild.name);
+
+        guild.channels.cache.forEach((channel) => {
+			console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+		})
+    });
+});
+
+client.on('message', (receivedMessage) => {
+    // Prevent bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+
+    receivedMessage.channel.send("Message received from " + receivedMessage.author.toString() + ": " + receivedMessage.content)
+})
+
+const bot_secret_token = "NzQxNzMxMjIwODIzNzM2Mzcw.Xy71Bw.TynnlTihgnBM7M82xXa0LZIwMnA";
+
+client.login(bot_secret_token);
